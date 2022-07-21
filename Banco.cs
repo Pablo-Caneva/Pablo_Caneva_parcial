c6413e7d6b93aae3fa6a2c4a9e05664a02ab2497
Pablo Caneva <pablo.caneva@frra.utn.edu.ar>
2022-07-21 16:09:57 -0300

    Agrega verificacion de cuentas y usuarios

--- Banco.cs
+++ Banco.cs
@@ -19,103 +19,277 @@ namespace Pablo_Caneva_parcial
         public decimal SaldoGeneral { get => saldoGeneral; set => saldoGeneral = value; }
         public List<Cliente> Clientes { get => clientes; set => clientes = value; }
 
+        public List<Cuenta> Cuentas { get => cuentas; set => cuentas = value; }
+
         public void AgregarCliente(string cuit, string nombre, string apellido, string condicionFiscal, decimal totalRecibos, bool recibos, string CBU, decimal saldo)
         {
-            this.saldoGeneral += saldo;
-            Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
-            Cuenta clienteca = new CajaDeAhorro(CBU, saldo);
-            clienteca.Usuarios.Add(cliente1);
-            cliente1.Cuentas.Add(clienteca);
-            this.clientes.Add(cliente1);
-            this.cuentas.Add(clienteca);
+            int indiceCliente = OperarCliente(cuit);
+            int indiceCuenta = OperarCuenta(CBU);
+            if ((indiceCliente == -1) && (indiceCuenta == -1))
+            {
+                this.saldoGeneral += saldo;
+                Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
+                Cuenta clienteca = new CajaDeAhorro(CBU, saldo);
+                clienteca.Usuarios.Add(cliente1);
+                cliente1.Cuentas.Add(clienteca);
+                this.clientes.Add(cliente1);
+                this.cuentas.Add(clienteca);
+            }
+            else if((indiceCuenta == -1) && (indiceCliente != -1))
+            {
+                this.saldoGeneral += saldo;
+                Cuenta clienteca = new CajaDeAhorro(CBU, saldo);
+                clienteca.Usuarios.Add(this.Clientes[indiceCliente]);
+                this.clientes[indiceCliente].Cuentas.Add(clienteca);
+                this.cuentas.Add(clienteca);
+            }
+            else if ((indiceCuenta != -1) && (indiceCliente == -1))
+            {
+                Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
+                cliente1.Cuentas.Add(this.Cuentas[indiceCuenta]);
+                this.Cuentas[indiceCuenta].Usuarios.Add(cliente1);
+                this.Clientes.Add(cliente1);
+            }
         }
 
         public void AgregarCliente(string cuit, string nombre, string apellido, string condicionFiscal, decimal totalRecibos, bool recibos, string CBU, decimal saldo, decimal descubierto)
         {
-            this.saldoGeneral += saldo;
-            Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
-            Cuenta clientecc = new CuentaCorriente(CBU, saldo, descubierto);
-            clientecc.Usuarios.Add(cliente1);
-            cliente1.Cuentas.Add(clientecc);
-            this.clientes.Add(cliente1);
-            this.cuentas.Add(clientecc);
+            int indiceCliente = OperarCliente(cuit);
+            int indiceCuenta = OperarCuenta(CBU);
+            if ((indiceCliente == -1) && (indiceCuenta == -1))
+            {
+                this.saldoGeneral += saldo;
+                Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
+                Cuenta clientecc = new CuentaCorriente(CBU, saldo, descubierto);
+                clientecc.Usuarios.Add(cliente1);
+                cliente1.Cuentas.Add(clientecc);
+                this.clientes.Add(cliente1);
+                this.cuentas.Add(clientecc);
+            }
+            else if ((indiceCuenta == -1) && (indiceCliente != -1))
+            {
+                this.saldoGeneral += saldo;
+                Cuenta clientecc = new CuentaCorriente(CBU, saldo, descubierto);
+                clientecc.Usuarios.Add(this.Clientes[indiceCliente]);
+                this.clientes[indiceCliente].Cuentas.Add(clientecc);
+                this.cuentas.Add(clientecc);
+            }
+            else if ((indiceCuenta != -1) && (indiceCliente == -1))
+            {
+                Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
+                cliente1.Cuentas.Add(this.Cuentas[indiceCuenta]);
+                this.Cuentas[indiceCuenta].Usuarios.Add(cliente1);
+                this.Clientes.Add(cliente1);
+            }
         }
 
         public void AgregarCliente(string cuit, string nombre, string apellido, string condicionFiscal, decimal totalRecibos, bool recibos, string CBU, decimal saldo, int dias, decimal total)
         {
-            this.saldoGeneral += saldo;
-            this.saldoGeneral += total;
-            Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
-            Cuenta clienteca = new CajaDeAhorro(CBU, saldo);
-            PlazoFijo pf = new PlazoFijo(dias, total);
-            clienteca.PlazoFijos.Add(pf);
-            clienteca.Usuarios.Add(cliente1);
-            cliente1.Cuentas.Add(clienteca);
-            this.clientes.Add(cliente1);
-            this.cuentas.Add(clienteca);
+            int indiceCliente = OperarCliente(cuit);
+            int indiceCuenta = OperarCuenta(CBU);
+            if ((indiceCliente == -1) && (indiceCuenta == -1))
+            {
+                this.saldoGeneral += saldo;
+                this.saldoGeneral += total;
+                Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
+                Cuenta clienteca = new CajaDeAhorro(CBU, saldo);
+                PlazoFijo pf = new PlazoFijo(dias, total);
+                clienteca.PlazoFijos.Add(pf);
+                clienteca.Usuarios.Add(cliente1);
+                cliente1.Cuentas.Add(clienteca);
+                this.clientes.Add(cliente1);
+                this.cuentas.Add(clienteca);
+            }
+            else if ((indiceCuenta == -1) && (indiceCliente != -1))
+            {
+                this.saldoGeneral += saldo;
+                this.saldoGeneral += total;
+                Cuenta clienteca = new CajaDeAhorro(CBU, saldo);
+                PlazoFijo pf = new PlazoFijo(dias, total);
+                clienteca.PlazoFijos.Add(pf);
+                clienteca.Usuarios.Add(this.Clientes[indiceCliente]);
+                this.clientes[indiceCliente].Cuentas.Add(clienteca);
+                this.cuentas.Add(clienteca);
+            }
+            else if ((indiceCuenta != -1) && (indiceCliente == -1))
+            {
+                Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
+                cliente1.Cuentas.Add(this.Cuentas[indiceCuenta]);
+                this.Cuentas[indiceCuenta].Usuarios.Add(cliente1);
+                this.Clientes.Add(cliente1);
+            }
         }
 
         public void AgregarCliente(string cuit, string nombre, string apellido, string condicionFiscal, decimal totalRecibos, bool recibos, string CBU, decimal saldo, decimal descubierto, int dias, decimal total)
         {
-            this.saldoGeneral += saldo;
-            this.saldoGeneral += total;
-            Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
-            Cuenta clientecc = new CuentaCorriente(CBU, saldo, descubierto);
-            PlazoFijo pf = new PlazoFijo(dias, total);
-            clientecc.PlazoFijos.Add(pf);
-            clientecc.Usuarios.Add(cliente1);
-            cliente1.Cuentas.Add(clientecc);
-            this.clientes.Add(cliente1);
-            this.cuentas.Add(clientecc);
+            int indiceCliente = OperarCliente(cuit);
+            int indiceCuenta = OperarCuenta(CBU);
+            if ((indiceCliente == -1) && (indiceCuenta == -1))
+            {
+                this.saldoGeneral += saldo;
+                this.saldoGeneral += total;
+                Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
+                Cuenta clientecc = new CuentaCorriente(CBU, saldo, descubierto);
+                PlazoFijo pf = new PlazoFijo(dias, total);
+                clientecc.PlazoFijos.Add(pf);
+                clientecc.Usuarios.Add(cliente1);
+                cliente1.Cuentas.Add(clientecc);
+                this.clientes.Add(cliente1);
+                this.cuentas.Add(clientecc);
+            }
+            else if ((indiceCuenta == -1) && (indiceCliente != -1))
+            {
+                this.saldoGeneral += saldo;
+                this.saldoGeneral += total;
+                Cuenta clientecc = new CuentaCorriente(CBU, saldo, descubierto);
+                PlazoFijo pf = new PlazoFijo(dias, total);
+                clientecc.PlazoFijos.Add(pf);
+                clientecc.Usuarios.Add(this.Clientes[indiceCliente]);
+                this.clientes[indiceCliente].Cuentas.Add(clientecc);
+                this.cuentas.Add(clientecc);
+            }
+            else if ((indiceCuenta != -1) && (indiceCliente == -1))
+            {
+                Cliente cliente1 = new Persona(cuit, nombre, apellido, condicionFiscal, totalRecibos, recibos);
+                cliente1.Cuentas.Add(this.Cuentas[indiceCuenta]);
+                this.Cuentas[indiceCuenta].Usuarios.Add(cliente1);
+                this.Clientes.Add(cliente1);
+            }
         }
 
         public void AgregarCliente(string cuit, string razonSocial, string condicionFiscal, bool balance, long nroIIBB, bool actaSocios, string CBU, decimal saldo)
         {
-            this.saldoGeneral += saldo;
-            Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
-            Cuenta empresaca = new CajaDeAhorro(CBU, saldo);
-            empresaca.Usuarios.Add(cliente1);
-            cliente1.Cuentas.Add(empresaca);
-            this.clientes.Add(cliente1);
-            this.cuentas.Add(empresaca);
+            int indiceCliente = OperarCliente(cuit);
+            int indiceCuenta = OperarCuenta(CBU);
+            if ((indiceCliente == -1) && (indiceCuenta == -1))
+            {
+                this.saldoGeneral += saldo;
+                Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
+                Cuenta empresaca = new CajaDeAhorro(CBU, saldo);
+                empresaca.Usuarios.Add(cliente1);
+                cliente1.Cuentas.Add(empresaca);
+                this.clientes.Add(cliente1);
+                this.cuentas.Add(empresaca);
+            }
+            else if ((indiceCuenta == -1) && (indiceCliente != -1))
+            {
+                this.saldoGeneral += saldo;
+                Cuenta empresaca = new CajaDeAhorro(CBU, saldo);
+                empresaca.Usuarios.Add(this.Clientes[indiceCliente]);
+                this.clientes[indiceCliente].Cuentas.Add(empresaca);
+                this.cuentas.Add(empresaca);
+            }
+            else if ((indiceCuenta != -1) && (indiceCliente == -1))
+            {
+                Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
+                cliente1.Cuentas.Add(this.Cuentas[indiceCuenta]);
+                this.Cuentas[indiceCuenta].Usuarios.Add(cliente1);
+                this.Clientes.Add(cliente1);
+            }
         }
 
         public void AgregarCliente(string cuit, string razonSocial, string condicionFiscal, bool balance, long nroIIBB, bool actaSocios, string CBU, decimal saldo, decimal descubierto)
         {
-            this.saldoGeneral += saldo;
-            Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
-            Cuenta empresacc = new CuentaCorriente(CBU, saldo, descubierto);
-            empresacc.Usuarios.Add(cliente1);
-            cliente1.Cuentas.Add(empresacc);
-            this.clientes.Add(cliente1);
+            int indiceCliente = OperarCliente(cuit);
+            int indiceCuenta = OperarCuenta(CBU);
+            if ((indiceCliente == -1) && (indiceCuenta == -1))
+            {
+                this.saldoGeneral += saldo;
+                Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
+                Cuenta empresacc = new CuentaCorriente(CBU, saldo, descubierto);
+                empresacc.Usuarios.Add(cliente1);
+                cliente1.Cuentas.Add(empresacc);
+                this.clientes.Add(cliente1);
+            }
+            else if ((indiceCuenta == -1) && (indiceCliente != -1))
+            {
+                this.saldoGeneral += saldo;
+                Cuenta empresacc = new CuentaCorriente(CBU, saldo, descubierto);
+                empresacc.Usuarios.Add(this.Clientes[indiceCliente]);
+                this.clientes[indiceCliente].Cuentas.Add(empresacc);
+                this.cuentas.Add(empresacc);
+            }
+            else if ((indiceCuenta != -1) && (indiceCliente == -1))
+            {
+                Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
+                cliente1.Cuentas.Add(this.Cuentas[indiceCuenta]);
+                this.Cuentas[indiceCuenta].Usuarios.Add(cliente1);
+                this.Clientes.Add(cliente1);
+            }
         }
 
         public void AgregarCliente(string cuit, string razonSocial, string condicionFiscal, bool balance, long nroIIBB, bool actaSocios, string CBU, decimal saldo, int dias, decimal total)
         {
-            this.saldoGeneral += saldo;
-            this.saldoGeneral += total;
-            Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
-            Cuenta empresaca = new CajaDeAhorro(CBU, saldo);
-            PlazoFijo pf = new PlazoFijo(dias, total);
-            empresaca.PlazoFijos.Add(pf);
-            empresaca.Usuarios.Add(cliente1);
-            cliente1.Cuentas.Add(empresaca);
-            this.clientes.Add(cliente1);
-            this.cuentas.Add(empresaca);
+            int indiceCliente = OperarCliente(cuit);
+            int indiceCuenta = OperarCuenta(CBU);
+            if ((indiceCliente == -1) && (indiceCuenta == -1))
+            {
+                this.saldoGeneral += saldo;
+                this.saldoGeneral += total;
+                Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
+                Cuenta empresaca = new CajaDeAhorro(CBU, saldo);
+                PlazoFijo pf = new PlazoFijo(dias, total);
+                empresaca.PlazoFijos.Add(pf);
+                empresaca.Usuarios.Add(cliente1);
+                cliente1.Cuentas.Add(empresaca);
+                this.clientes.Add(cliente1);
+                this.cuentas.Add(empresaca);
+            }
+            else if ((indiceCuenta == -1) && (indiceCliente != -1))
+            {
+                this.saldoGeneral += saldo;
+                this.saldoGeneral += total;
+                Cuenta empresaca = new CajaDeAhorro(CBU, saldo);
+                PlazoFijo pf = new PlazoFijo(dias, total);
+                empresaca.PlazoFijos.Add(pf);
+                empresaca.Usuarios.Add(this.Clientes[indiceCliente]);
+                this.clientes[indiceCliente].Cuentas.Add(empresaca);
+                this.cuentas.Add(empresaca);
+            }
+            else if ((indiceCuenta != -1) && (indiceCliente == -1))
+            {
+                Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
+                cliente1.Cuentas.Add(this.Cuentas[indiceCuenta]);
+                this.Cuentas[indiceCuenta].Usuarios.Add(cliente1);
+                this.Clientes.Add(cliente1);
+            }
         }
 
         public void AgregarCliente(string cuit, string razonSocial, string condicionFiscal, bool balance, long nroIIBB, bool actaSocios, string CBU, decimal saldo, decimal descubierto, int dias, decimal total)
         {
-            this.saldoGeneral += saldo;
-            this.saldoGeneral += total;
-            Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
-            Cuenta empresacc = new CuentaCorriente(CBU, saldo, descubierto);
-            PlazoFijo pf = new PlazoFijo(dias, total);
-            empresacc.PlazoFijos.Add(pf);
-            empresacc.Usuarios.Add(cliente1);
-            cliente1.Cuentas.Add(empresacc);
-            this.clientes.Add(cliente1);
-            this.cuentas.Add(empresacc);
+            int indiceCliente = OperarCliente(cuit);
+            int indiceCuenta = OperarCuenta(CBU);
+            if ((indiceCliente == -1) && (indiceCuenta == -1))
+            {
+                this.saldoGeneral += saldo;
+                this.saldoGeneral += total;
+                Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
+                Cuenta empresacc = new CuentaCorriente(CBU, saldo, descubierto);
+                PlazoFijo pf = new PlazoFijo(dias, total);
+                empresacc.PlazoFijos.Add(pf);
+                empresacc.Usuarios.Add(cliente1);
+                cliente1.Cuentas.Add(empresacc);
+                this.clientes.Add(cliente1);
+                this.cuentas.Add(empresacc);
+            }
+            else if ((indiceCuenta == -1) && (indiceCliente != -1))
+            {
+                this.saldoGeneral += saldo;
+                this.saldoGeneral += total;
+                Cuenta empresacc = new CuentaCorriente(CBU, saldo, descubierto);
+                PlazoFijo pf = new PlazoFijo(dias, total);
+                empresacc.PlazoFijos.Add(pf);
+                empresacc.Usuarios.Add(this.Clientes[indiceCliente]);
+                this.clientes[indiceCliente].Cuentas.Add(empresacc);
+                this.cuentas.Add(empresacc);
+            }
+            else if ((indiceCuenta != -1) && (indiceCliente == -1))
+            {
+                Cliente cliente1 = new Empresa(cuit, razonSocial, condicionFiscal, balance, nroIIBB, actaSocios);
+                cliente1.Cuentas.Add(this.Cuentas[indiceCuenta]);
+                this.Cuentas[indiceCuenta].Usuarios.Add(cliente1);
+                this.Clientes.Add(cliente1);
+            }
         }
 
         public int OperarCliente(string CUIT)
@@ -124,6 +298,16 @@ namespace Pablo_Caneva_parcial
             {
                 if (cliente.CUIT1 == CUIT)
                     return Clientes.IndexOf(cliente);
+            }
+            return -1;
+        }
+
+        public int OperarCuenta(string CBU)
+        {
+            foreach (var cuenta in Cuentas)
+            {
+                if (cuenta.CBU1 == CBU)
+                    return Cuentas.IndexOf(cuenta);
             }
             return -1;
         }
